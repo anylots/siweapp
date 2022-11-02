1d59bb9323a915d4aa84bc98709a83b7b782a5b3
anylots <842567494@qq.com>
2022-11-02 10:28:20 +0800

    address_recovery

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::str::FromStr;
 
-use async_std::io::ReadExt;
 use ethers;
 use ethers::core::types::H160;
 use ethers::signers::{LocalWallet, Signer};
@@ -10,12 +9,10 @@ use siwe::Message;
 
 #[async_std::main]
 async fn main() {
-    println!("SIWE app");
+    println!("start SIWE app");
 
-    sign_verify().await;
-
-    // siwe_msg();
-    // siwe_test();
+    let result = sign_verify().await;
+    assert!(result.is_ok());
 }
 
 async fn sign_verify() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,10 +21,11 @@ async fn sign_verify() -> Result<(), Box<dyn std::error::Error>> {
     // instantiate the wallet
     let wallet = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7"
         .parse::<LocalWallet>()?;
-
     let address = wallet.address();
     let address_hex = hex::encode(H160::as_bytes(&address).to_vec());
     println!("{}", address_hex);
+
+    //0x63F9725f107358c9115BC9d86c72dD5823E9B1E6
     let string_message = r#"service.org wants you to sign in with your Ethereum account:
 0x63F9725f107358c9115BC9d86c72dD5823E9B1E6
 
@@ -42,64 +40,29 @@ Resources:
 - ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/
 - https://example.com/my-web2-claim.json"#;
 
-    //63f9725f107358c9115bc9d86c72dd5823e9b1e6
-    //0x63F9725f107358c9115BC9d86c72dD5823E9B1E6
-
     //sign a message
     let signature: Signature = wallet.sign_message(string_message).await?;
-
     let result = signature.recover(string_message);
-    // println!("{}", result.unwrap());
-    let address_hex1 = hex::encode(H160::as_bytes(&result.unwrap()).to_vec());
-    println!("{}", address_hex1);
+    let address_hex_recover = hex::encode(H160::as_bytes(&result.unwrap()).to_vec());
+    println!("{}", address_hex_recover);
 
     let verify_result = signature.verify(string_message, wallet.address()).unwrap();
-
+    
     println!("{}", "verify");
 
-    let msg = Message::from_str(string_message).unwrap();
-
+    let siwe_msg = Message::from_str(string_message).unwrap();
     let sig = <[u8; 65]>::from(signature);
-
-    let a: Vec<u8> = Vec::from(signature);
 
     println!("{}", "verify_eip191 start");
 
-    // if let Err(e) = message.verify(&signature).await {
-    //     // message cannot be correctly authenticated at this time
-    // }
-    let signer: Vec<u8> = msg.verify_eip191(&sig)?;
+    let signer: Vec<u8> = siwe_msg.verify_eip191(&sig)?;
+    let public_key = &signer[1..];
+    let process_sha3 = Keccak256::default().chain_update(public_key).finalize();
+    let result_slice = process_sha3.as_slice();
+    let address_recovery = hex::encode(&result_slice[12..]);
+
+    println!("{}", address_recovery);
     println!("{}", "verify_eip191 end");
 
-    let test = &signer[1..];
-
-    let address123 = Keccak256::default().chain_update(test);
-
-    let mut a1 = address123.finalize();
-    let mut a2 = a1.as_slice();
-    let s1 = hex::encode(&a2[12..]);
-    // let b1 = a.bytes();
-
-    // println!("{}", hex::encode(address.finalize()));
-
     Ok(())
-}
-
-fn siwe_test() {
-    let message = Message::from_str(
-        r#"localhost:4361 wants you to sign in with your Ethereum account:
-0x4f8828d8CE3906cFe31381eB2a8aC4ADe601C36F
-
-SIWE Notepad Example
-
-URI: http://localhost:4361
-Version: 1
-Chain ID: 1
-Nonce: kEWepMt9knR6lWJ6A
-Issued At: 2021-12-07T18:28:18.807Z"#,
-    )
-    .unwrap();
-
-    println!("{}", "verify_eip191:");
-    println!("{}", "verify_eip191:");
 }
