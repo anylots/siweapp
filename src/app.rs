ac24c94a9a07ce5e0ebac8834448c1b979b359bb
anylots <842567494@qq.com>
2023-03-30 23:33:49 +0800

    AddExtensionLayer

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,9 @@ use ethers::types::Signature;
 use serde::{Deserialize, Serialize};
 use siwe::Message;
 use siwe::VerificationOpts;
+use siweapp::limiter;
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct SignRequest {
@@ -12,15 +14,33 @@ struct SignRequest {
     address: String,
 }
 
+
+// pub fn init(){
+//     let mut bucket = token_bucket.lock().unwrap();
+//     let result = bucket.acquire(1);
+// }
+
 /**
  * sign in service
  */
 pub async fn process_sign_in(param: String) -> String {
+    let limiter = limiter::new_limiter();
+    let mut bucket = limiter.lock().unwrap();
+    let result = bucket.acquire(1);
+    if !result {
+        return "Please try again later".to_string();
+    }
+
     println!("process_sign_in");
     println!("{}", param);
     let sign_request: SignRequest = serde_json::from_str(param.as_str()).unwrap();
-    if let Err(e) = verify_siwe(&sign_request.message, sign_request.sig, &sign_request.address,
-    ).await{
+    if let Err(e) = verify_siwe(
+        &sign_request.message,
+        sign_request.sig,
+        &sign_request.address,
+    )
+    .await
+    {
         return e.to_string();
     }
 
--- src/limiter.rs
+++ src/limiter.rs
@@ -1,14 +1,14 @@
-use std::time::{Instant, Duration};
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
-struct TokenBucket {
+pub struct TokenBucket {
     capacity: u64,
     tokens: u64,
     last_update: Instant,
 }
 
 impl TokenBucket {
-    fn new(capacity: u64) -> Self {
+    pub fn new(capacity: u64) -> Self {
         TokenBucket {
             capacity,
             tokens: capacity,
@@ -16,7 +16,7 @@ impl TokenBucket {
         }
     }
 
-    fn acquire(&mut self, tokens: u64) -> bool {
+    pub fn acquire(&mut self, tokens: u64) -> bool {
         self.update_tokens();
         if tokens > self.tokens {
             return false;
@@ -32,6 +32,16 @@ impl TokenBucket {
         self.tokens = std::cmp::min(self.tokens + tokens_generated, self.capacity);
         self.last_update = now;
     }
+}
+
+// 使用 Arc 和 Mutex 来定义全局变量 my_global_var
+lazy_static::lazy_static! {
+    pub static ref token_bucket: Arc<Mutex<TokenBucket>> =
+        Arc::new(Mutex::new(TokenBucket::new(100)));
+}
+
+pub fn new_limiter() -> Arc<Mutex<TokenBucket>> {
+    return token_bucket.clone();
 }
 
 fn main() {
